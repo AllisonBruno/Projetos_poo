2c79f827775d0041a5d72f5c17000655794db9a4
Alisson <allison.bruno1994@gmail.com>
2023-05-17 16:56:15 -0300

    Projeto Banco do Brasil
    implementado os metodos (depositar, sacar, consultar saldo), na classe conta bancaria

--- Banco Brasil/src/banco/brasil/BancoBrasil.java
+++ Banco Brasil/src/banco/brasil/BancoBrasil.java
@@ -1,9 +1,9 @@
 package banco.brasil;
+
 import java.util.Scanner;
 
 public class BancoBrasil {
 
-    
     public static void main(String[] args) {
         //Instanciando um objeto do tipo contaBancaria e referenciando nas variaveis contaBancaria1 e contaBancaria2\\
         ContaBancaria contaBancaria1 = new ContaBancaria();
@@ -21,20 +21,29 @@ public class BancoBrasil {
         + contaBancaria1.proprietario + "\n"
         + contaBancaria1.saldo);
         //System.out.println(contaBancaria1.toString());
-        */
+         */
 
-        
-       Scanner scan = new Scanner(System.in);
-       System.out.println("Bem vindo ao Banco do Brasil");
-       System.out.println("Cadastro de contas");
-       System.out.println("Digite a agencia ->");
-       contaBancaria1.agencia = scan.next();
-       System.out.println("Digite a conta ->");
-       contaBancaria1.conta = scan.next();
-       System.out.println("Digite o proprietario ->");
-       contaBancaria1.proprietario = scan.next();
-       System.out.println("Digite saldo inicial ->");
-       contaBancaria1.saldo = scan.nextDouble();
-       
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bem vindo ao Banco do Brasil");
+        System.out.println("Cadastro de contas");
+        System.out.println("Digite a agencia ->");
+        contaBancaria1.agencia = scan.next();
+        System.out.println("Digite a conta ->");
+        contaBancaria1.conta = scan.next();
+        System.out.println("Digite o proprietario ->");
+        contaBancaria1.proprietario = scan.next();
+        System.out.println("Digite o valor de deposito ->");
+        contaBancaria1.depositar(scan.nextDouble());
+
+        System.out.println(contaBancaria1.agencia + "\n"
+                + contaBancaria1.conta + "\n"
+                + contaBancaria1.proprietario + "\n"
+                + contaBancaria1.consultarSaldo());
+
+        System.out.println("\n\nDigite o valor para saque ->");
+        contaBancaria1.sacar(scan.nextDouble());
+
+        System.out.println(contaBancaria1.consultarSaldo());
+
     }
 }
--- Banco Brasil/src/banco/brasil/ContaBancaria.java
+++ Banco Brasil/src/banco/brasil/ContaBancaria.java
@@ -7,18 +7,25 @@ public class ContaBancaria {
     double saldo;
     String proprietario;
 
-    void depositar() {
+    void depositar(double valor) {//Metodo com parametro
+        this.saldo += valor;
     }
 
-    void sacar() {
+    void sacar(double valor) {
+
+        if (valor < saldo) {
+            this.saldo -= valor;
+        } else {
+            System.out.println("Fundo insuficiente para efetuar o saque");
+        }
     }
 
     void transferir() {
     }
 
-    String consultarSaldo() {
-        return "";
-    }
-    
-}
+    String consultarSaldo() {//Metodo sem paramentro
+        //String.format() método da classe String para utilizar formatações com identificadores(%d, %f, %s).
+        return String.format("Seu saldo é: R$ %.2f", this.saldo);
 
+    }
+}
